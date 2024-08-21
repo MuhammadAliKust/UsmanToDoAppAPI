6cba9c66fe92b9c9e5902c8f5c12df7256da8a44
Muhammad Ali Nizami <kustalics@gmail.com>
2024-08-21 17:12:34 +0500

    Auth APIs Done

---
+++ lib/services/auth.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:todo_app_api/models/login.dart';
+import 'package:todo_app_api/models/user.dart';
+
+import '../models/register_user.dart';
+
+class AuthServices {
+  ///Register
+  Future<RegisterResponseModel> registerUser(
+      {required String email,
+      required String password,
+      required String name}) async {
+    http.Response response = await http.post(
+        Uri.parse('https://todo-nu-plum-19.vercel.app/users/register'),
+        body: {"name": name, "email": email, "password": password});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return RegisterResponseModel.fromJson(jsonDecode(response.body));
+    } else {
+      return RegisterResponseModel();
+    }
+  }
+
+  ///Login
+  Future<LoginResponseModel> loginUser(
+      {required String email,
+      required String password,
+      required String name}) async {
+    http.Response response = await http.post(
+        Uri.parse('https://todo-nu-plum-19.vercel.app/users/login'),
+        body: {"name": name, "email": email, "password": password});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return LoginResponseModel.fromJson(jsonDecode(response.body));
+    } else {
+      return LoginResponseModel();
+    }
+  }
+
+  ///Get User Data
+  Future<UserModel> getUserProfile({required String token}) async {
+    http.Response response = await http.get(
+        Uri.parse('https://todo-nu-plum-19.vercel.app/users/login'),
+        headers: {'Authorization': token});
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      return UserModel.fromJson(jsonDecode(response.body));
+    } else {
+      return UserModel();
+    }
+  }
+}
